686533ccc1f746152987213f13c68838c9bc9e82
INDRA SUNNY <107390281+indrasunny@users.noreply.github.com>
2022-06-14 10:57:36 +0800

    aturcara import random ramalan scroce MAS vs VIET
    PENGGGUNA ANTARA ATURCARA RANDOM

---
+++ MAS vs VIET.py
@@ -0,0 +1,22 @@
+import random
+
+# User-defined function untuk simulasi RAMALAN SCORE MALAYSIA VS VIETNAM
+def jaring_gol(x):
+    hasil = (random.randint(0,x))
+    return hasil
+
+# Membuat panggilan function
+# Hantar nilai bilangan batu yang digunakan ke dalam bilangan_gol()
+terus = "Y"
+while terus == "Y":
+    hasil_timbang = jaring_gol(5)
+    print("Hasil gol MALAYSIA ialah "+str(hasil_timbang)+" jaringan")
+
+# Menangkap batu.Membuat panggilan fungsi.
+# Hantar hasil timbangan ke dalam bilangan_gol()
+    if hasil_timbang >0:
+      tangkap = jaring_gol(hasil_timbang)
+      print("Hasil gol VIETNAM ialah "+str(tangkap)+" jaringan")
+      
+    terus=input("\nTeruskan(Y)atau Berhenti(T)? Tekan (Y/T)")
+    print("  ")
